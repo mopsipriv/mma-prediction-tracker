3afd20a899701010595112d21a9b625e134117cd
mopsipriv <jarvinenandrei@gmail.com>
2026-08-19 23:07:13 +0300

    New struct of fights

--- internal/models/fight.go
+++ internal/models/fight.go
@@ -4,10 +4,10 @@ import "time"
 
 type Fight struct {
 	ID          int64     `json:"id"`
-	EventID     int64     `json:"event_id"`
+	EventID     *int64    `json:"event_id,omitempty"`
 	Fighter1ID  int64     `json:"fighter_1_id"`
 	Fighter2ID  int64     `json:"fighter_2_id"`
-	WinnerID    *int64    `json:"winner_id"`
+	WinnerID    *int64    `json:"winner_id,omitempty"`
 	WeightClass string    `json:"weight_class"`
 	CreatedAt   time.Time `json:"created_at"`
 }
