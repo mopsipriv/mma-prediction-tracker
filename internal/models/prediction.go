893b4df9095f60e5ec3fb26d1275c26ca8aac14e
mopsipriv <jarvinenandrei@gmail.com>
2026-08-19 23:40:23 +0300

    Prediction structure

--- internal/models/prediction.go
+++ internal/models/prediction.go
@@ -1,1 +1,12 @@
 package models
+
+import "time"
+
+type Prediction struct {
+	ID                int64     `json:"id"`
+	PredictorID       int64     `json:"predictor_id"`
+	FightID           int64     `json:"fight_id"`
+	PredictedWinnerID int64     `json:"predicted_winner_id"`
+	Probability       float64   `json:"probability"`
+	CreatedAt         time.Time `json:"created_at"`
+}
